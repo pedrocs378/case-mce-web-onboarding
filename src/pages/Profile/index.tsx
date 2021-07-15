91215efa3d1b196c159b3d9f52bdc9ab04558efa
Pedro César <pedrocs378@gmail.com>
2021-07-15 01:14:03 -0300

    feat: adiciona campos na tela de perfil

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,11 +1,64 @@
+import { FormEvent, useState } from 'react'
+
+import { Button } from '../../components/Button'
+import { Input } from '../../components/Input'
+import { InputPassword } from '../../components/Input/InputPassword'
+
+import cameraIcon from '../../assets/icons/camera-edit-icon.svg'
 
 import * as S from './styles'
 
 export function Profile() {
+	const [username, setUsername] = useState('')
+	const [phone, setPhone] = useState('')
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
+
+	function handleSaveChanges(event: FormEvent) {
+		event.preventDefault()
+	}
 
 	return (
 		<S.Container>
-			Perfil
+			<main>
+				<form onSubmit={handleSaveChanges}>
+					<S.AvatarContainer>
+						<img src="http://www.github.com/pedrocs378.png" alt="Pedro César" />
+
+						<label htmlFor="avatar">
+							<input type="file" name="avatar" id="avatar" />
+							<img src={cameraIcon} alt="Icone Camera" />
+						</label>
+					</S.AvatarContainer>
+
+					<Input
+						name="username"
+						placeholder="Nome do usuário"
+						value={username}
+						onChange={event => setUsername(event.target.value)}
+					/>
+					<Input
+						name="phone"
+						placeholder="Número de telefone"
+						value={phone}
+						onChange={event => setPhone(event.target.value)}
+					/>
+					<Input
+						name="email"
+						placeholder="E-mail"
+						value={email}
+						onChange={event => setEmail(event.target.value)}
+					/>
+					<InputPassword
+						name="password"
+						placeholder="Senha"
+						value={password}
+						onChange={event => setPassword(event.target.value)}
+					/>
+
+					<Button type="submit">Salvar alterações</Button>
+				</form>
+			</main>
 		</S.Container>
 	)
 }
--- src/pages/Profile/styles.ts
+++ src/pages/Profile/styles.ts
@@ -2,4 +2,74 @@ import styled from "styled-components";
 
 export const Container = styled.div`
 	height: calc(100vh - 8.75rem);
+
+	main {
+		width: 100%;
+		max-width: 32.1rem;
+		margin: 0 auto;
+		padding: 1.375rem 6.25rem 5rem;
+		border-radius: 0.75rem;
+		background: var(--white);
+
+		box-shadow: 0px 17px 8.486px rgba(239, 241, 249, 0.75);
+
+		form {
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+
+			> label + label {
+				margin-top: 1.375rem;
+			}
+
+			button[type="submit"] {
+				margin-top: 3rem;
+			}
+		}
+	}
+`
+
+export const AvatarContainer = styled.div`
+	margin: 1rem 0 3.125rem 0;
+	position: relative;
+
+	> img {
+		height: 6.8rem;
+		width: 6.8rem;
+
+		border-radius: 50%;
+	}
+
+	label {
+		height: 2rem;
+		width: 2rem;
+
+		border: 2px solid var(--white);
+		border-radius: 50%;
+
+		position: absolute;
+		bottom: 0;
+		right: 0;
+
+		background: var(--orange);
+		display: flex;
+		align-items: center;
+		justify-content: center;
+
+		cursor: pointer;
+		transition: filter 0.2s;
+
+		input {
+			display: none;
+		}
+
+		img {
+			height: 1rem;
+			width: 1rem;
+		}
+
+		&:hover {
+			filter: brightness(0.95);
+		}
+	}
 `
